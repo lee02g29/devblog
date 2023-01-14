772e6ce87b1dd78ea4c31239b9f9e064f71a5476
MSDN-SPECIAL\Administrator <lee02g29@naver.com>
2023-01-15 06:01:01 +0900

    new project commit

---
+++ devblog/devblog/src/test/java/com/perdy/devblog/DevblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perdy.devblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
